322503765db1efdb67d7b77d925ace7dceb0690c
Hasalapera <hasalashe777@gmail.com>
2026-08-11 23:31:46 +0530

    some files created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from './pages/Login'
+import Register from './pages/Register'
+import NotFound from './pages/NotFound'
 function App() {
 
   return (
-    <>
-    <h1 className='mt-8 bg-slate-300 text-green-600'>Hola Mundo</h1>
-    </>
+      <Routes>
+        {/* Root path eka login ekata redirect karanawa */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        
+        {/* Pages routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+        {/* Match wenne nathi ona url ekakata 404 page eka pennanawa */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
   )
 }
 
